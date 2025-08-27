7e69a14fe20ba0a72d027f60c138aaa0ba86e4d7
Chameleon Cloud User <Soujanya Ponnapalli>
2025-08-27 23:09:29 +0000

    :tada: Script to install dependencies

---
+++ install-deps.sh
@@ -0,0 +1,6 @@
+mkdir raft-engines
+
+git clone https://github.com/etcd-io/etcd.git raft-engines/etcd-raft
+git clone https://github.com/hashicorp/raft.git raft-engines/hashicorp-raft
+git clone https://github.com/tikv/tikv.git raft-engines/tikv
+git clone https://github.com/tikv/raft-rs.git raft-engines/tikv-raft
