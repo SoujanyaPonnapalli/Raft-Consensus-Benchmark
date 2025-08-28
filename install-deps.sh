a6fda95c39f41dd1c034bde10045c519a769b03f
Soujanya Ponnapalli <soujanyap95@gmail.com>
2025-08-28 18:35:41 +0000

    :tada: Updating raft engines and configs

--- install-deps.sh
+++ install-deps.sh
@@ -4,3 +4,4 @@ git clone https://github.com/etcd-io/etcd.git raft-engines/etcd-raft
 git clone https://github.com/hashicorp/raft.git raft-engines/hashicorp-raft
 git clone https://github.com/tikv/tikv.git raft-engines/tikv
 git clone https://github.com/tikv/raft-rs.git raft-engines/tikv-raft
+git clone https://github.com/redis/redis.git raft-engines/redis
